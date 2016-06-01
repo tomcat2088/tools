048d2c997ea2e41ad840aa12b0a7087fafe63adf
tomcat2088 <tomcat1991@126.com>
2016-06-01 18:47:16 +0800

    update nginxcli & pip3 install

--- NginxCli/main.py
+++ NginxCli/main.py
@@ -1,27 +1,67 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 from jinja2 import Template
 import sys
+import subprocess
+from nginx_conf import *
 
 server_name = '127.0.0.1'
 server_port = '8000'
 document_root = '/usr/local/'
 
-def find_nginx_conf():
-	
-
 def gen_server_config():
 	with open("php.template", 'r') as file:
 		content = file.read();
 		template = Template(content)
-		print(template.render(server_name = server_name,server_port = server_port,document_root = document_root))
+		return (template.render(server_name = server_name,server_port = server_port,document_root = document_root))
 
-usage = 'Usage:'+sys.argv[0] + ' server_name server_port document_root'
+# usage = 'Usage:'+sys.argv[0] + ' server_name server_port document_root'
 
-if len(sys.argv) <= 4:
-	print(usage)
-	gen_server_config()
-else:
-	server_name = sys.argv[1]
-	server_port = sys.argv[2]
-	document_root = sys.argv[3]
-	gen_server_config()
+# if len(sys.argv) <= 4:
+# 	print(usage)
+# 	gen_server_config()
+# else:
+# 	server_name = sys.argv[1]
+# 	server_port = sys.argv[2]
+# 	document_root = sys.argv[3]
+# 	gen_server_config()
+
+def validation():
+	if len(server_name) > 0 and len(server_port) > 0 and len(document_root) > 0:
+		return True
+	return False
+
+print("Welcome to Nginx Cli v1.0.0")
+vhost_path = check_nginx_conf()
+if vhost_path is None:
+	print('Start Failed!!!')
+
+while 1:
+	word = input('>>');
+	if word == 'q':
+		break
+	elif word == 'i':
+		show_nginx_conf()
+	elif word == 'ls':
+		list_servers(vhost_path)
+	elif word == 'create':
+		server_name = input("Input server_name: ")
+		server_port = input("Input server_port: ")
+		document_root = input("Input www_root: ")
+		if validation():
+			content = gen_server_config()
+			print("Preview:\n")
+			print(content)
+			choice = input("Want to use this? (yes | no): ")
+			if choice == 'yes' or choice == 'y':
+				create_server(server_name,content,vhost_path)
+	elif word == 'rm':
+		server_name = input("Input server_name: ")
+		choice = input("Really want to remove this? (yes | no): ")
+		if choice == 'yes' or choice == 'y':
+			remove_server(server_name,vhost_path)
+	elif word == 'reload':
+		subprocess.call(['nginx','-s','stop'])
+		subprocess.call(['nginx'])
+
+
+
---
+++ NginxCli/nginx_conf.py
@@ -0,0 +1,77 @@
+import subprocess
+import re
+from pathlib import Path
+import os
+
+nginx_conf_path = None
+
+def check_nginx_conf():
+	print("Check environment ...")
+	prefix = '--conf-path='
+	conf_file = 'nginx.conf'
+	p = subprocess.Popen('nginx -V', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	lines = p.stdout.read().decode('utf-8')
+	regex_str = '--conf-path([a-zA-Z=//.]*)'
+	m = re.search(regex_str,lines)
+	if m:
+		path = m.group(0)[len(prefix):]
+		global nginx_conf_path
+		nginx_conf_path = Path(path)
+		fix_conf_file(path)
+		conf_dir = path[:len(path) - len(conf_file)]
+		print('Info: nginx.conf path is :' + path)
+		return check_vhost(conf_dir)
+	else:
+		print("Error: nginx may not installed.")
+
+def show_nginx_conf():
+	global nginx_conf_path
+	if nginx_conf_path:
+		print(nginx_conf_path.read_text())
+
+def fix_conf_file(conf_file_path):
+	content = Path(conf_file_path).read_text()
+	if content.find('include vhosts') >= 0:
+		print("Info: nginx.conf pass validation.")
+	else:
+		print('Warning: nginx.conf need <include vhosts/*>,fixing...')
+		with open(str(conf_file_path),'w+') as file:
+			right_index = content.rfind('}')
+			content = content[0:right_index]
+			content = content+'include vhosts/*; \n}'
+			file.write(content)
+
+def check_vhost(conf_dir):
+	conf_dir_path = Path(conf_dir)
+	vhost_path = conf_dir_path / 'vhosts'
+	if vhost_path.exists():
+		print('Info: vhosts exists.')
+	else:
+		print('Error: vhosts not exists , creating ...')
+		vhost_path.mkdir()
+	return vhost_path
+
+def list_servers(vhosts_path):
+	print('You have following servers:')
+	for x in os.listdir(str(vhosts_path)):
+		print(x)
+
+def create_server(name,conf_content,vhost_path):
+	if vhost_path:
+		conf_path = vhost_path / (name + '.conf')
+		print(conf_path)
+		if conf_path.exists():
+			print('Server is already exists!!!')
+		else:
+			conf_path.touch()
+			conf_path.write_text(conf_content)
+
+def remove_server(name,vhost_path):
+	if vhost_path:
+		conf_path = vhost_path / (name + '.conf')
+		print(conf_path)
+		if conf_path.exists() == False:
+			print('Server is not exists!!!')
+		else:
+			conf_path.unlink()
+			print('{0} remove completed.'.format(name))
