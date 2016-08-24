b6c72fabd86c579f0ba84c24baba54c90617e4cd
wang yang <wangyang@wangdeMacBook-Pro.local>
2016-08-25 00:22:26 +0800

    almost there

--- Clis/CrashReportCli/iosdevice.py
+++ Clis/CrashReportCli/iosdevice.py
@@ -1,14 +1,14 @@
 #!/usr/local/bin/python3
+# coding: utf-8
 
 from util import *
 import json
 import io,os
 import time
 import re
+import datetime
 
-# collect crash report for iOS simulator
-
-class Simulator():
+class IOSDevice():
     def __init__(self,name,udid):
         self.name = name
         self.udid = udid
@@ -25,7 +25,7 @@ def list_devices():
         if ret:
             name = ret.group(1)
             udid = ret.group(2)
-            simulators.append(Simulator(name,udid))
+            simulators.append(IOSDevice(name, udid))
     return simulators
 
 def list_crash_report(simulator_name,app_bundle_name):
@@ -33,19 +33,27 @@ def list_crash_report(simulator_name,app_bundle_name):
     print(os.listdir(crash_dir))
 
 def run_app(device_id,app_bundle_name):
-    print('{0} run start'.format(app_bundle_name))
+    print('[{0}] Launch at {1}'.format(app_bundle_name,datetime.datetime.now().strftime('%H:%M:%S:%s')))
     automation_template_path = '/Applications/Xcode.app/Contents/Applications/Instruments.app/Contents/PlugIns/AutomationInstrument.xrplugin/Contents/Resources/Automation.tracetemplate'
     default_uia_file = 'automation.js'
-    output_path = './output/'
-    command = 'instruments -w {0} -t {1} {2} -e UIASCRIPT {3} -e UIARESULTSPATH {4}'.format(device_id,automation_template_path,app_bundle_name,default_uia_file,output_path)
+    output_path = '/tmp/traces/'
+    if os.path.exists(output_path) == False:
+        os.mkdir(output_path)
+    trace_file_name = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '.trace'
+    output_path = os.path.join(output_path,trace_file_name)
+    print('Save trace to ' + output_path)
+    command = 'instruments -w {0} -t {1} -D {4} {2} -e UIASCRIPT {3} '.format(device_id,automation_template_path,app_bundle_name,default_uia_file,output_path)
     ret = exec_wait(command)
-    print('{0} run finish'.format(app_bundle_name))
+    print('[{0}] Exit at {1}'.format(app_bundle_name,datetime.datetime.now().strftime('%H:%M:%S:%s')))
 
 def run_app_repeat(device_id,app_bundle_name,launch_count,kill_delay):#kill_delay单位是s
-    while launch_count > 0:
+    run_count = 1
+    while launch_count > 0 or launch_count < 0:
+        print('Run #{0}'.format(run_count))
         launch_count = launch_count - 1
         run_app(device_id,app_bundle_name)
         time.sleep(kill_delay)
+        run_count = run_count + 1
 
 
 
--- Clis/CrashReportCli/main.py
+++ Clis/CrashReportCli/main.py
@@ -2,60 +2,100 @@
 
 import sys
 import getopt
-import simulator
+import iosdevice
 import crlogger
 
-usage = '''Usage: {0} [options] <app bundle id>
+usage = '''Usage: {0} [options] <action name> arguments
 
-Run specific App on simulator & collect the crashreport
+Run specific App on iDevice(iPhone|Simulator) & collect the crashreport
 
 Options:
-  -h, --help                  show this help message and exit
-  --version                   show version
-  -u, --udid                  use simulator with udid
-  -v, --verbose               enable verbose log output
+    -h, --help                  show this help message and exit
+    --version                   show version
+    -u, --udid                  use device with udid
+    -v, --verbose               enable verbose log output
+
+Actions:
+    ls                                          list devices
+    run <app bundle id> <repeat count> <delay>  run app on device.if repeat count < 0,run forever
+
 '''
 
+
 def show_usage():
-    print(usage.format(sys.argv[0]))
+	print(usage.format(sys.argv[0]))
+	exit(2)
 
 def show_version():
-    print('0.0.1')
+	print('0.0.1')
+
 
 def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"hu:v",["help","version","udid=","verbose"])
-    except getopt.GetoptError:
-        show_usage()
-        sys.exit(2)
+	try:
+		opts, args = getopt.getopt(argv, "hu:v", ["help", "version", "udid=", "verbose"])
+	except getopt.GetoptError:
+		show_usage()
+		sys.exit(2)
 
-    udid = ''
-    for opt, arg in opts:
-        if opt in ['-h','--help']:
-            show_usage()
-        if opt in ['-v','--verbose']:
-            crlogger.logger.enable()
-        if opt in ['--version']:
-            show_version()
-        if opt in ['-u','--udid']:
-            udid = arg
+	udid = ''
+	option_last_index = 0
+	for opt, arg in opts:
+		if opt in ['-h', '--help']:
+			show_usage()
+		if opt in ['-v', '--verbose']:
+			crlogger.logger.enable()
+		if opt in ['--version']:
+			show_version()
+		if opt in ['-u', '--udid']:
+			udid = arg
+		if opt != '':
+			option_last_index = option_last_index + 1
+		if arg != '':
+			option_last_index = option_last_index + 1
 
+	app_bundle_id = ''
+	action = ''
+	repeat_count = 0
+	delay = 2
+	if option_last_index < len(argv):
+		action = argv[option_last_index]
+	if action == 'run':
+		if option_last_index + 1 < len(argv):
+			app_bundle_id = argv[option_last_index + 1]
+		if app_bundle_id == '':
+			show_usage()
+		if option_last_index + 2 < len(argv):
+			repeat_count = int(argv[option_last_index + 2])
+		if option_last_index + 3 < len(argv):
+			delay = int(argv[option_last_index + 3])
+		if udid != '':
+			print('Running with device udid : {0}'.format(udid))
+		else:
+			print('Select device:')
+			index = 1
+			devices = iosdevice.list_devices()
+			for device in devices:
+				print('{0}: {1}'.format(index, device.debug_description()))
+				index = index + 1
+			result = input('>> ')
+			select_device_index = int(result) - 1
+			if select_device_index >= 0 and select_device_index < len(devices):
+				print('Select device {0}'.format(devices[select_device_index].debug_description()))
+				udid = devices[select_device_index].udid
+			else:
+				print('invalid device')
+				exit(2)
 
-    if udid != '':
-        print('running with udid : {0}'.format(udid))
-    else:
-        print('select simulator:')
-        index = 1
-        devices = simulator.list_devices()
-        for device in devices:
-            print('{0}: {1}'.format(index,device.debug_description()))
-            index = index + 1
-        result = input('>> ')
-        select_device_index = int(result) - 1
-        if select_device_index >=0 and select_device_index < len(devices):
-            print('select device {0}'.format(devices[select_device_index].debug_description()))
-            simulator.run_app(devices[select_device_index].udid,'com.dx168.epmyg')
-            simulator.run_app_repeat(devices[select_device_index].udid,'com.dx168.epmyg',4,2)
+		if repeat_count != 0:
+			iosdevice.run_app_repeat(udid, app_bundle_id, repeat_count, delay)
+		else:
+			iosdevice.run_app(udid, app_bundle_id)
+	elif action == 'ls':
+		devices = iosdevice.list_devices()
+		for device in devices:
+			print(device.debug_description())
+	else:
+		show_usage()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
