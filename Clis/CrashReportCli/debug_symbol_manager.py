df19609483a7d6335056ebac877539d1369e164f
tomcat2088 <tomcat1991@126.com>
2016-08-30 18:08:26 +0800

    force symbolicate

--- Clis/CrashReportCli/debug_symbol_manager.py
+++ Clis/CrashReportCli/debug_symbol_manager.py
@@ -5,8 +5,14 @@ import datetime
 
 dst_package_path = os.path.expanduser('~/Documents/Packages/')
 
+#TODO: 优化匹配或者使用最新的
 def find_match_symbol_or_app(app_name):
-
+    package_dir = os.path.join(dst_package_path, app_name)
+    files = os.listdir(package_dir)
+    for file in files:
+        if file.find('symbol') > 0:
+            return os.path.join(package_dir, file)
+    return ''
 
 if __name__ == '__main__':
     src_dir = sys.argv[1]
@@ -16,14 +22,14 @@ if __name__ == '__main__':
         os.mkdir(package_dir)
 
     print(product_name)
-    src_dysm_path = os.path.join(src_dir,product_name + '.app.dSYM')
-    src_app_path = os.path.join(src_dir, product_name + '.app')
+    src_dysm_path = os.path.join(src_dir,product_name + '.app.dSYM/Contents/Resources/DWARF/' + product_name)
+    src_app_path = os.path.join(src_dir, product_name + '.app/' + product_name)
     print(src_app_path)
     print(src_dysm_path)
     if os.path.exists(src_dysm_path):
-        new_pack_name = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + ".app.dSYM"
+        new_pack_name = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + ".symbol"
         os.system('cp -r {0} {1}'.format(src_dysm_path, os.path.join(package_dir,new_pack_name)))
     elif os.path.exists(src_app_path):
-        new_pack_name = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + ".app"
+        new_pack_name = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + ".symbol"
         print('cp -r {0} {1}'.format(src_app_path,new_pack_name))
         os.system('cp -r {0} {1}'.format(src_app_path,os.path.join(package_dir,new_pack_name)))
--- Clis/CrashReportCli/symbolicate.py
+++ Clis/CrashReportCli/symbolicate.py
@@ -1,20 +1,43 @@
 import io
 import re
 import debug_symbol_manager
+import json
+import os
+import util
 
 def process_crash(crash_file):
+    symbolicatecrash = '/Applications/Xcode.app/Contents/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/symbolicatecrash'
+    # use symbolicatecrash to process system backtrace
+    util.exec_wait('{0} {1} > {2}.tmp'.format(symbolicatecrash,crash_file,crash_file))
+    util.exec_wait('mv {1}.tmp {2}'.format(symbolicatecrash, crash_file, crash_file))
+
     file = open(crash_file)
+    first_line = file.readline()
+    info = json.loads(first_line)
+    app_name = info['app_name']
+    symbol_file = debug_symbol_manager.find_match_symbol_or_app(app_name)
+    if os.path.exists(symbol_file) == False:
+        print('No dSYM File Found!')
+        exit(2)
+    print('Find dSYM in {0}'.format(symbol_file))
     while True:
         line = file.readline()
         if len(line) == 0:
             break
-        ret = re.match(r'^\d+\s+(\w+)\s+(\w+)\s+(\w+)',line)
+        re_str = r'^\d+\s+(\w+)\s+(\w+)\s+(\w+)'
+        ret = re.match(re_str,line)
         if ret:
-            app_name = ret.group(1)
-            address = ret.group(2)
-            base_address = ret.group(3)
+            name = ret.group(1)
+            if name == app_name:
+                address = ret.group(2)
+                base_address = ret.group(3)
+                result = util.exec_ret('atos -arch arm64 -o "{0}" -l {1} {2}'.format(symbol_file,base_address,address))
+                line = re.sub(r'\s+(\w+ \+ \w+)',r' {0}'.format(result),line)
+                print(line,end="")
+        else:
+            print(line,end="")
     file.close()
 
 
 if __name__ == '__main__':
-    process_crash('/tmp/crashes/d28744372ac8bb0d2ef26a8c994ce6f5dca06c1a/2016-08-26_17:48:17:1472204897/CoreDumpDemo-2016-08-26-174705.ips')
+    process_crash('/tmp/crashes/d28744372ac8bb0d2ef26a8c994ce6f5dca06c1a/2016-08-26_13:53:06:1472190786/CoreDumpDemo-2016-08-26-135242.ips')
